913382dc19833f0328480948a16987d21b075e73
EvgeniyPetrovF <evgeniy.petrov@fojin.tech>
2023-03-01 17:50:56 +0300

    feat: add logout

--- src/features/Settings/screens/SettingsScreen/index.tsx
+++ src/features/Settings/screens/SettingsScreen/index.tsx
@@ -1,12 +1,21 @@
 import React, {FC} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useMMKVString} from 'react-native-mmkv';
 import TextWrapper from '../../../../components/TextWrapper';
 import {styles} from './styles';
+import CustomButton from '../../../../components/CustomButton';
 
 const SettingsScreen: FC = () => {
+  const [username, setUsername] = useMMKVString('nickname');
+
+  const handleLogout = () => {
+    setUsername(undefined);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <TextWrapper>Settings!</TextWrapper>
+      <TextWrapper style={styles.username}>{username}</TextWrapper>
+      <CustomButton label={'Logout'} onPress={handleLogout} />
     </SafeAreaView>
   );
 };
